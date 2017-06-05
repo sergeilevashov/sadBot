8a01f1dd04ce022e17fa616fa681355d279455b2
sergeilevashov <sergeiglevashov@gmail.com>
2017-06-05 01:35:00 -0700

    complete key hiding

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from urllib.request import urlopen
 import urllib
 import re
 import tweepy
+import keys
 
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+access_token= keys.access_token
+access_token_secret= keys.access_token_secret
 url = "http://twitter.com/realDonaldTrump"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
