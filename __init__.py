d46de7f4881f954d0fc4d680ca9fc1e84813dde5
sergeilevashov <sergeiglevashov@gmail.com>
2017-06-05 23:13:33 -0700

    added loop and option to quit

--- __init__.py
+++ __init__.py
@@ -17,16 +17,18 @@ access_token_secret= keys.access_token_secret
 def findSad(tweet):
     return re.compile(r'\b({0})\b'.format(tweet), flags=re.IGNORECASE).search
 
-user_input = input('enter the word to search for:')
+while user_input != 'q':
+    user_input = input('enter the word to search for, or q to quit: ')
 
-url = "http://twitter.com/realDonaldTrump"
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-maxtweets = api.user_timeline(user_id = '25073877' ,count=200)
-for status in maxtweets:
-    if findSad(user_input)(status.text.lower()):
-        print(status.text)
-        flag = True
-if not flag:
-    print('he hasnt said that word... at least not on twitter')
+    url = "http://twitter.com/realDonaldTrump"
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    maxtweets = api.user_timeline(user_id = '25073877' ,count=200)
+    for status in maxtweets:
+        if findSad(user_input)(status.text.lower()) and user_input != 'q':
+            print(status.text)
+            flag = True
+    if not flag and user_input != 'q':
+        print('he hasnt said that word... at least not on twitter')
+    flag = False
